1e6210f3c282752ec812426a523f8baa08ae1246
toddcoles <todd_coles@hotmail.com>
2020-08-08 15:02:49 -0700

    Removing node_modules

---
+++ drawer/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MiniDrawer from './Minidrawer'
+
+function App() {
+  return (
+    <div className="App">
+    test
+    <MiniDrawer />
+    </div>
+  );
+}
+
+export default App;
