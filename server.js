22519c36c9c6066c0d0adbc3118e86c1d96d294e
toddcoles <todd_coles@hotmail.com>
2021-03-16 17:32:54 -0700

    Update server.js
    Body-Parser deprication

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 // require express
-var express = require("express");
+const express = require("express");
+
 // path module -- try to figure out where and why we use this
-var path = require("path");
+const path = require("path");
+
 // create the express app
-var app = express();
-var bodyParser = require('body-parser');
+const app = express();
+app.use(express.json({ urlencoded: true}));
 // use it!
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true })); Depricated
+
 // static content
 app.use(express.static(path.join(__dirname, "./static")));
+
 // setting up ejs and our views folder
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
+
 // root route to render the index.ejs view
 app.get('/', function(req, res) {
  res.render("index");
 })
+
 // post route for adding a user
 app.post('/users', function(req, res) {
  console.log("POST DATA", req.body);
@@ -23,6 +29,7 @@ app.post('/users', function(req, res) {
  // Then redirect to the root route
  res.redirect('/');
 })
+
 // tell the express app to listen on port 8000
 app.listen(8000, function() {
  console.log("listening on port 8000");
